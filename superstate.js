19c34856cb6a47325bcb42c7b5067ef93681bdf7
Eric Vicenti <ericvicenti@gmail.com>
2014-04-08 16:45:07 -0700

    fix issue when saving encoded path in browser document cookie

--- superstate.js
+++ superstate.js
@@ -1,4 +1,3 @@
-var CookieCutter = require('cookie-cutter');
 var _ = require('underscore');
 
 var superstate = module.exports = {};
@@ -15,6 +14,36 @@ function deserialize(base64) {
   var string = buffer.toString();
   var data = JSON.parse(string);
   return data;
+}
+
+function CookieCutter(doc) {
+  if (!doc) doc = {};
+  if (typeof doc === 'string') doc = { cookie: doc };
+  if (doc.cookie === undefined) doc.cookie = '';
+  
+  var self = {};
+  self.get = function (key) {
+    var splat = doc.cookie.split(/;\s*/);
+    for (var i = 0; i < splat.length; i++) {
+      var ps = splat[i].split('=');
+      var k = unescape(ps[0]);
+      if (k === key) return unescape(ps[1]);
+    }
+    return undefined;
+  };
+  
+  self.set = function (key, value, opts) {
+    if (!opts) opts = {};
+    var s = escape(key) + '=' + escape(value);
+    if (opts.expires) s += '; expires=' + opts.expires;
+    // path is not well-respected on chrome when escaped
+    if (opts.path) s += '; path=' + opts.path;
+    // if (opts.path) s += '; path=' + escape(opts.path);
+    doc.cookie = s;
+    return s;
+  };
+
+  return self;
 }
 
 var commonMethods = {
