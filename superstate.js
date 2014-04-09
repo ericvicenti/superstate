d41c99c87839210f7be52fa22615ffbd56e6d17e
Eric Vicenti <ericvicenti@gmail.com>
2014-04-09 11:13:50 -0700

    switch to cookies-js for browser cookie storage

--- superstate.js
+++ superstate.js
@@ -1,4 +1,5 @@
 var _ = require('underscore');
+var BrowserCookies = require('cookies-js');
 
 var superstate = module.exports = {};
 
@@ -14,36 +15,6 @@ function deserialize(base64) {
   var string = buffer.toString();
   var data = JSON.parse(string);
   return data;
-}
-
-function CookieCutter(doc) {
-  if (!doc) doc = {};
-  if (typeof doc === 'string') doc = { cookie: doc };
-  if (doc.cookie === undefined) doc.cookie = '';
-  
-  var self = {};
-  self.get = function (key) {
-    var splat = doc.cookie.split(/;\s*/);
-    for (var i = 0; i < splat.length; i++) {
-      var ps = splat[i].split('=');
-      var k = unescape(ps[0]);
-      if (k === key) return unescape(ps[1]);
-    }
-    return undefined;
-  };
-  
-  self.set = function (key, value, opts) {
-    if (!opts) opts = {};
-    var s = escape(key) + '=' + escape(value);
-    if (opts.expires) s += '; expires=' + opts.expires;
-    // path is not well-respected on chrome when escaped
-    if (opts.path) s += '; path=' + opts.path;
-    // if (opts.path) s += '; path=' + escape(opts.path);
-    doc.cookie = s;
-    return s;
-  };
-
-  return self;
 }
 
 var commonMethods = {
@@ -64,19 +35,14 @@ superstate.browser = function(document, stateName) {
 
   var superstate = this;
 
-  var browserCookie = CookieCutter(document);
-
-  var currentCookie = browserCookie.get(stateName);
+  var currentCookie = BrowserCookies.get(stateName);
 
   var state = currentCookie ? deserialize(currentCookie) : {};
 
   this.setState = function setState(data) {
     state = data;
     currentCookie = serialize(data);
-    var cookieOpts = {
-      path: '/'
-    };
-    browserCookie.set(stateName, currentCookie, cookieOpts);
+    BrowserCookies.set(stateName, currentCookie);
   }
 
   this.getState = function getState() {
@@ -114,5 +80,3 @@ superstate.express = function(req, res, stateName) {
 
   return superstate;
 }
-
-
